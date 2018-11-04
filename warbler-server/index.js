f1256fe893643a32ef3180bfe60250b3b88b24c0
Neiva07 <lucasneiva07@gmail.com>
2018-11-04 15:31:26 -0300

    fixed bad commits

---
+++ warbler-server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser')
+
+const PORT = 8081;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+//routes will come here.
+
+
+app.use((req,res,next) => {
+    let err = new Error('Not Founded');
+    err.status = 404;
+    next(err);
+})
+
+app.listen(PORT, () => {
+    console.log(`Server starting on port ${PORT}`)
+})
